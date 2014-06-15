46a3060f98a053752ae0c3355f34e7f89c0264a1
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-15 16:37:12 +0800

    定义数据结构

---
+++ Data.h
@@ -0,0 +1,21 @@
+﻿/*
+**链式队列的数据结构
+*/
+#ifndef DATA_H
+#define DATA_H
+
+typedef int Elem;
+
+typedef struct Node
+{
+	Elem data;	/*数据域*/
+	struct Node *pNext;	/*指针域*/
+}NODE;
+
+typedef struct LinkQueue
+{
+	NODE *front;	/*队头,指向头结点*/
+	NODE *rear;	/*队尾,指向尾部数据*/
+}LINKQUEUE;
+
+#endif
