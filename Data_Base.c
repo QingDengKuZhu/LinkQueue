faae51f6bccb1f5a99e6e757508ac449f459b989
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-16 10:27:35 +0800

    实现队列的初始化

---
+++ Data_Base.c
@@ -0,0 +1,18 @@
+﻿#include "Data.h"
+#include "Main_First.h"
+#include "Data_Base.h"
+
+void InitQueue(LINKQUEUE *pQ)
+{
+	pQ->front = (NODE *)malloc(sizeof(NODE));
+	if (!pQ->front)
+	{
+		printf("动态内存分配失败!\n");
+		exit(ERROR);
+	}
+
+	pQ->rear = pQ->front;
+
+	return;
+}
+
