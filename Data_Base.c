b067d580919c7e4a407491a484115435fe4685cb
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-16 16:05:52 +0800

    1.实现队列的清空操作 2.修改bug

--- Data_Base.c
+++ Data_Base.c
@@ -19,13 +19,37 @@ void InitQueue(LINKQUEUE *pQ)
 void DestroyQueue(LINKQUEUE *pQ)
 {
 	NODE *p = pQ->front;
-	while (!p)
+	while (p)
 	{
 		pQ->front = p->pNext;
 		free(p);
 		p = pQ->front;
 	}/*退出while循环时,front为NULL*/
 	pQ->rear = NULL;
+
+	return;
+}
+
+void ClearQueue(LINKQUEUE *pQ)
+{
+	NODE *p = pQ->front;
+	NODE *q = NULL;
+	
+	if (!p)	/*如果队列不存在(即已经被销毁)*/
+	{
+		printf("队列已经被销毁,无法执行清空操作!\n");
+		return;
+	}
+	q = p->pNext;
+
+	while (q)
+	{
+		p = q->pNext;
+		free(q);
+		p = q;
+	}
+	pQ->front->pNext = NULL;
+	pQ->rear = pQ->front;
 
 	return;
 }
