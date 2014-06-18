de042c62077fc998344b1cd427fcae24e5ae9c34
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-06-18 10:37:04 +0800

    测试

---
+++ Test.c
@@ -0,0 +1,81 @@
+﻿#include "Test.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+
+void gotoxy(const short x, const short y)
+{
+	COORD pos;	/*COORD在windows.h中定义*/
+	pos.X = x;
+	pos.Y = y;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); /* 设置光标位置 */
+
+	return;
+}
+
+
+void Menu(void)
+{
+	system("cls");
+	gotoxy(10, 5);                        /*在文本窗口中设置光标*/
+	printf("			链式队列测试			");
+	gotoxy(8, 8);
+	printf("-----------------------------Menu-----------------------------\n");
+	gotoxy(8, 9);
+	printf("| 1.入队                     2.输出队列                  |\n");
+	gotoxy(8, 10);
+	printf("| 3.出队                     4.输出表长                  |\n");
+	gotoxy(8, 11);
+	printf("| 5.清空                     6.队头                      |\n");
+	gotoxy(8, 12);
+	printf("| 7.队列是否为空             8.销毁队列            |\n");
+	gotoxy(8, 13);
+	printf("--------------------------------------------------------------\n");
+	
+    return;
+}
+
+size_t InputPos(const char notice[])
+{
+	int pos;		/*保存待输入的位序*/
+	unsigned n;		/*保存scanf的返回值*/
+	
+	do 
+	{
+		printf(notice);
+		n = scanf("%d", &pos);
+		fflush(stdin);
+
+		/*判断输入的位序是否在合理的范围内*/
+		if (pos<1 || n!=1)
+		{
+			printf("位序必须大于0!!!\n");
+		}
+	} while (pos<1 || n!=1);
+
+	return (size_t)pos;
+}
+
+int InputValue(const char notice[])
+{	
+	int e;
+	unsigned n;
+
+	do 
+	{
+		printf(notice);
+		n = scanf("%d", &e);
+		fflush(stdin);
+
+		if (n != 1)
+		{
+			printf("请输入满足条件的数值!\n");
+		}
+
+	} while (n!=1);
+	
+	return e;
+}
---
+++ main.c
@@ -0,0 +1,133 @@
+﻿/**********************
+**主函数, 测试
+***********************/
+
+#include "Main_First.h"
+#include "Data.h"
+#include "Data_Base.h"
+#include "Test.h"
+#include <time.h> /*随机数*/
+
+int main(void)
+{
+	int select;		/*保存选择变量*/
+	size_t pos;		/*位序*/
+	Elem e;			/*保存从函数返回的结点的值*/
+	Elem v;			/*保存传递给函数的结点的值*/
+	
+	size_t i= 0;
+	LINKQUEUE Q;
+	InitQueue(&Q);
+	srand((int)time(NULL));
+	while (i < 10)
+	{
+		InsertQueue(&Q, rand()%20);
+		++i;
+	}
+
+	while (1)	/*while_@1*/
+	{
+		if (!Q.front)
+		{
+			printf("队列不存在!\n");
+			break;
+		}
+
+		system("cls");
+		Menu();
+
+		printf("请输入您的选择(1~10):");
+		scanf("%d", &select);
+		getchar();
+
+		switch (select)	/*switch_@1*/
+		{
+		case 1:			/*入队*/
+			v = InputValue("入队元素为:");
+			if (FAILE == InsertQueue(&Q, v))
+			{
+				printf("入队失败!\n");
+			}
+			else
+			{
+				printf("入队成功!\n");
+			}
+			
+			getchar();
+			break;
+		case 2:			/*输出队列*/
+			printf("队列为:");
+			TraveQueue(&Q);
+			
+			getchar();
+			break;
+		case 3:			/*出队*/
+			if (OK == DeleteQueue(&Q, &e))
+			{
+				printf("出队成功,删除的元素是%d!\n", e);
+			}
+			else
+			{
+				printf("删除失败!\n");
+			}
+
+			getchar();
+			break;
+
+		case 4:			/*输出队列的长度*/
+			printf("表长为: %d \n", QueueLength(&Q));
+			
+			getchar();
+			break;
+
+		case 5:			/*清空队列*/
+			ClearQueue(&Q);
+			printf("该表已经清空!\n");
+			
+			getchar();
+			break;
+
+		case 6:			/*返回队头元素*/
+			if (OK == GetHead(&Q, &e))
+			{
+				printf("该结点为:%d\n", e);
+			}
+			else
+			{
+				printf("不存在!\n");
+			}
+
+			getchar();	
+			break;
+
+		case 7:			/*判断队列是否为空*/
+			if (QueueEmpty(&Q) == TRUE)
+			{
+				printf("队列为空!\n");
+			}
+			else
+			{
+				printf("队列非空!\n");
+			}
+
+			getchar();	
+			break;
+		
+		case 8:			/*销毁队列*/
+			DestroyQueue(&Q);
+			printf("队列已删除!\n");
+
+			getchar();	
+			break;
+		
+		default:
+			printf("请重新选择!\n");
+			
+			getchar();
+			break;
+		}/*switch_@1*/
+
+	}	/*while_@1*/
+	
+	return EXIT_SUCCESS;
+}
